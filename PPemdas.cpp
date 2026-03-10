67a2928fb8c2ca0232cb276eedc94b0773f2341f
r4zkaaa <rzkiiahmd11@gmail.com>
2026-03-10 20:57:19 +0700

    Third

--- PPemdas.cpp
+++ PPemdas.cpp
@@ -5,3 +5,8 @@ float bb, tb;
 
 void inputbmi()
  {
+    cout << "\n--- Selamat Datang Mahasiswa Ti Kelas C 2026 ---" << endl;
+    cout << "Masukkan Berat Badan (kg): ";
+    cin >> bb;
+    cout << "Masukkan Tinggi Badan (m): ";
+    cin >> tb;
